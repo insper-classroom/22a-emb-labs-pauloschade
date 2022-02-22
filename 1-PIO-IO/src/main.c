4b67a171037c680c93f3d030457755f844b22b79
pauloschade <paulochade0505@gmail.com>
2022-02-22 11:37:34 -0300

    lab 1

--- 1-PIO-IO/src/main.c
+++ 1-PIO-IO/src/main.c
@@ -23,10 +23,20 @@
 /* defines                                                              */
 /************************************************************************/
 
-#define LED_PIO           
-#define LED_PIO_ID        
-#define LED_PIO_IDX       
-#define LED_PIO_IDX_MASK  
+//LED
+#define LED_PIO   PIOC  
+// #      
+#define LED_PIO_ID    ID_PIOC
+#define LED_PIO_IDX   8    
+#define LED_PIO_IDX_MASK (1u << LED_PIO_IDX)
+
+//BUT
+#define BUT_PIO		PIOA
+#define BUT_PIO_ID	ID_PIOA
+#define BUT_PIO_IDX 11
+#define BUT_PIO_IDX_MASK (1 << BUT_PIO_IDX)
+
+
 
 /************************************************************************/
 /* constants                                                            */
@@ -53,7 +63,21 @@ void init(void);
 // Função de inicialização do uC
 void init(void)
 {
-
+	sysclk_init();
+	
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	
+	//LED
+	pmc_enable_periph_clk(LED_PIO_ID);
+	//PC8 as output
+	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0 ,0);
+	
+	//BUT
+	pmc_enable_periph_clk(BUT_PIO_ID);
+	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 1);
+	
 }
 
 /************************************************************************/
@@ -69,6 +93,20 @@ int main(void)
   // aplicacoes embarcadas não devem sair do while(1).
   while (1)
   {
+	  if(!pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK))
+	  {
+		for (int i = 0; i < 5; i++)
+		{
+			// 1 on LED Pin
+			pio_set(PIOC, LED_PIO_IDX_MASK);
+			delay_ms(200);
+			// 0 on LED Pin
+			pio_clear(PIOC, LED_PIO_IDX_MASK);
+			delay_ms(200);
+		}
+	  } else {
+			pio_clear(PIOC, LED_PIO_IDX_MASK);
+	  }
 
   }
   return 0;
